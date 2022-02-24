a940cb2e989f123a6d9b67a251cb259e04278095
Lakshan Deshapriya <49018033+Lakdesh96@users.noreply.github.com>
2022-02-24 14:37:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
   echo "hi" 
+    echo "hii"
    
-function familyName($fname, $year) {
-  echo "$fname Refsnes. Born in $year <br>";
-}
-
-familyName("Hege", "1975");
-familyName("Stale", "1978");
-familyName("Kai Jim", "1983");
 
   
 ?>
